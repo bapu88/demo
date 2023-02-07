a9eb995adc612b5ae04b909d19f30b4316f72773
ArtiMakwana <arti@appmatictech.com>
2023-02-07 14:25:44 +0530

    changes from dev_new branch

--- file2.php
+++ file2.php
@@ -1,9 +1,9 @@
 <?php
-    echo "Hello";
-    echo "Hello12";
-    echo "Hello13";
-    echo "Hello14";
-    echo "Hello2";
-    echo "Hello3";
-    echo "Hello4";
+    class Demo
+    {
+        public function demo_name()
+        {
+            return [];
+        }
+    }
 ?>
